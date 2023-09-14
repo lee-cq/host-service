421e1b7cf3d8a9b4df734840841dff28c73f92ce
LeeCQ <lee-cq@qq.com>
2023-09-14 15:21:25 +0800

    更新依赖和boot脚本

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,31 @@
 
 cd "$(dirname $0)" || exit
 
-alias python=python3
+
+_py="$(which python3.11)"
+PYTHON=${PYTHON_EXE:-${_py}}
+if [ "${PYTHON}x" == "x" ];then
+  echo "目前需要安装python3.11或者如果不在PATH中，使用 PYTHON_EXE 环境变量指定. " >&2
+  exit 1
+fi
 
 if [ ! -d venv ];then
-  python -m venv venv
+  echo "创建虚拟环境 ..."
+  $PYTHON -m venv venv
 fi
 
 source venv/bin/activate
 
 if [ ! -f .requirements-installed ];then
+  if [ "$(pip config get global.index-url)" == "" ]; then
+    echo "设置镜像源： https://pypi.tuna.tsinghua.edu.cn/simple"
+    pip config set global.index-url "https://pypi.tuna.tsinghua.edu.cn/simple"
+  fi
+  echo "安装依赖 ..."
   pip install -r requirements.txt
   pip install -r requirements-dev.txt
   echo > .requirements-installed
 fi
-
-
 
 action=$1
 shift
