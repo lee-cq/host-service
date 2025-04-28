71a660994f8eea2ecf4fd7a61a77f5065e9c3421
Lee CQ <lee_cq@icloud.com>
2025-04-28 10:55:53 +0800

    Build Ubuntu fixbug

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # 初始化脚本
 
+function fix_chmod() {
+    find /etc/ -type d -exec chmod ug+x {} \;
+}
 
+fix_chmod
 echo Inited.
 # 应在最后启动 Supervisord
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
